1c3c4fa11849fac53cf71e87c044cb1e1b37253d
Sudhir <I45069@verisk.com>
2026-01-01 16:32:36 +0530

    Observer

--- DesignPatterns/Observer/Observer.cs
+++ DesignPatterns/Observer/Observer.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns.Observer
 {
-    internal class Observer
+    public interface ISubscriber//observer
     {
+        void Update(String ChannelName, String VideoTitle);
+    }
+    public interface IYouTubeChannel
+    {
+        void Subscribe(ISubscriber subscriber);
+        void Unsubscribe(ISubscriber subscriber);
+        void UploadVideo(String VideoTitle);
+        void NotifySubscribers();
+    }
+    public class YouTubeChannel : IYouTubeChannel
+    {
+        public String ChannelName { get; set; }
+        public String VideoTitle { get; set; }
+        public List<ISubscriber> subscribers = new List<ISubscriber>();
+        //public List<ISubscriber> subscribers = new(); both are same
+        public YouTubeChannel(String ChannelName)
+        {
+            this.ChannelName = ChannelName;
+        }
+
+        public void Subscribe(ISubscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void Unsubscribe(ISubscriber subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+        public void UploadVideo(string VideoTitle)
+        {
+            this.VideoTitle = VideoTitle;
+            NotifySubscribers();
+        }
+        public void NotifySubscribers()
+        {
+            foreach (var subscriber in subscribers)
+            {
+                subscriber.Update(ChannelName, VideoTitle);
+            }
+        }
+    }
+    public class UserSubscriber : ISubscriber
+    {
+        public string userName { get; set; }
+        public UserSubscriber(string username)
+        {
+            this.userName = username;
+        }
+
+        public void Update(string ChannelName, string VideoTitle)
+        {
+            Console.WriteLine(userName + " Got Notified");
+        }
+    }
+    internal class ProgramOP
+    {
+        static void MainOP(string[] args)
+        {
+            var channel = new YouTubeChannel("Sudhir Goud");
+            var user1 = new UserSubscriber("Sudhir");
+            var user2 = new UserSubscriber("bang");
+            channel.Subscribe(user1);
+            channel.Subscribe(user2);
+            channel.UploadVideo("new video");
+            Console.Read();
+        }
     }
 }
